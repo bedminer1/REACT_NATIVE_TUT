307e577e08d7de4426ef2521b7242f34f12d1405
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-04-19 00:14:47 +0800

    learnt about image component from react-native

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,16 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { StatusBar } from 'expo-status-bar';
+
+const logoImg = require("./assets/favicon.png")
 
 export default function App() {
   return (
-  <View className="flex-1 justify-center items-center bg-purple-800">
+  <View className="items-center justify-center flex-1 bg-purple-700">
     <StatusBar style="auto" />
-    <Text>Hello World</Text>
-    <View className="w-20 h-20 bg-blue-600">
-
-    </View>
+    <Text className="text-5xl font-bold text-white">Hello World</Text>
+    <Image source={logoImg} className="w-20 h-20"/>
   </View>
   )
 }
+
 
 
